bfc3e9a7d0e0d9a602d792a364bba47c8c29419b
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-01-30 20:14:57 +0800

    Initial getter and setter for numerator - automatically converts to lowest terms

--- Fraction.py
+++ Fraction.py
@@ -1,21 +1,32 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
+        # TODO
         pass
 
     def gcd(a, b):
-        #TODO
+        # TODO
         pass
 
+    @property
+    def numerator(self):
+        return self._numerator
+
+    @numerator.setter
+    def numerator(self, new_numerator):
+        gcd = Fraction.gcd(new_numerator, self.denominator)
+        self._numerator = new_numerator // gcd
+        self._denominator //= gcd
+
     def get_numerator(self):
-        #TODO
+        # TODO
         pass
 
     def get_denominator(self):
-        #TODO
+        # TODO
         pass
 
     def get_fraction(self):
-        #TODO
+        # TODO
         pass
+
